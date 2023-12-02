2d6ca3aa475cd40abe3407fe81e8c6a9868cebd0
jatinrajput3758 <jatinrajput3758@gmail.com>
2023-12-02 11:08:33 +0530

    initial commit

---
+++ JavaScript/script.js
@@ -0,0 +1,4 @@
+var dark_btn = document.getElementById("dark_btn");
+dark_btn.onclick = function () {
+    document.body.classList.toggle("dark_mode_on")
+}
---
+++ Script.js
@@ -0,0 +1,8 @@
+(function ($) {
+    new WOW().init();
+})(jQuery);
+
+var dark_btn = document.getElementById("dark_btn");
+dark_btn.onclick = function () {
+    document.body.classList.toggle("dark_mode_on")
+}
